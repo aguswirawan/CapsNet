51eb7ce0e0b460bfec6f6ac5c1f5e3f2dcfcd9cd
aguswirawan <imade.aguswirawan@mail.ugm.ac.id>
2022-02-20 12:47:53 +0700

    tambah import tensorflow

--- capsulenet-multi-gpu_4class_ver5.py
+++ capsulenet-multi-gpu_4class_ver5.py
@@ -12,6 +12,7 @@ Usage:
 from tensorflow.keras import backend as K
 from tensorflow.keras import layers, models, optimizers,regularizers
 from capsulelayers import CapsuleLayer, PrimaryCap, Length, Mask
+import tensorflows as tf
 
 K.set_image_data_format('channels_last')
 
