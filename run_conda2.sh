57f588bc712d72e4b66541f3bc71d502b638ef81
aguswirawan <imade.aguswirawan@mail.ugm.ac.id>
2022-02-21 15:42:00 +0700

    membagi data

--- run_conda2.sh
+++ run_conda2.sh
@@ -6,9 +6,9 @@
 #
 #SBATCH --nodes=1
 #SBATCH --time=24:00:00
-#SBATCH --nodelist=komputasi03
+#SBATCH --nodelist=komputasi07
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/home/m450296/miniconda3/envs/virtenv/lib
 
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate virtenv
-python capsulenet-multi-gpu_4class_group.py
+python capsulenet-multi-gpu_4class_group2.py
