1b96a2cf0517878373dd8b3b54d94e23e3eb4ca1
“Agus <imade.aguswirawan@mail.ugm.ac.id>
2022-02-21 12:16:02 +0700

    edit run conda

--- run_conda.sh
+++ run_conda.sh
@@ -7,7 +7,7 @@
 #SBATCH --nodes=1
 #SBATCH --time=24:00:00
 #SBATCH --nodelist=komputasi07
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${CONDA_PREFIX}/lib
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/home/m450296/miniconda3/envs/virtenv/lib
 
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate virtenv
