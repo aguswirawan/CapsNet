88415659e89d7aa47357c3176cb47c127700dbf4
“Agus <imade.aguswirawan@mail.ugm.ac.id>
2022-03-28 06:22:31 +0700

    primer

--- run_MWMF3_G1.sh
+++ run_MWMF3_G1.sh
@@ -5,8 +5,8 @@
 #SBATCH --error=logs/capsnet_%A.err
 #
 #SBATCH --nodes=1
-#SBATCH --time=24:00:00
-#SBATCH --nodelist=komputasi05
+#SBATCH --time=50:00:00
+#SBATCH --nodelist=komputasi06
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/home/m450296/miniconda3/envs/virtenv/lib
 
 source ~/miniconda3/etc/profile.d/conda.sh
