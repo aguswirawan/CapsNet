f38e92442426572f242d50400a29ed78cca0d6e7
“Agus <imade.aguswirawan@mail.ugm.ac.id>
2022-04-01 16:38:25 +0700

    Primer v1

--- run_primer_MWMF.sh
+++ run_primer_MWMF.sh
@@ -6,7 +6,7 @@
 #
 #SBATCH --nodes=1
 #SBATCH --time=50:00:00
-#SBATCH --nodelist=komputasi05
+#SBATCH --nodelist=komputasi06
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/home/m450296/miniconda3/envs/virtenv/lib
 
 source ~/miniconda3/etc/profile.d/conda.sh
